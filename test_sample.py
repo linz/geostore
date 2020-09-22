aca514376632fbbd19127556756b579d7d1ddb03
Ivan Mincik <ivan.mincik@gmail.com>
2020-09-22 11:30:05 +0200

    fix: fix Python code formatting

--- test_sample.py
+++ test_sample.py
@@ -3,6 +3,7 @@ Sample test for Test Python file. Remove this file once some real Python code
 will appear in this repository.
 """
 
+
 def test_i_am_ok():
     """Sample test"""
     assert "a" == "a"
