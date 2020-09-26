ded441b2e181cd8216619581ef70178d7bb418ca
Simon Planzer <splanzer@linz.govt.nz>
2020-09-26 21:02:54 +1200

    feat(infra): intialise CDK project
    closes #4

---
+++ infra/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+from data_stores.data_lake_stack import DataLakeStack
+
+app = core.App()
+
+DataLakeStack(
+    app,
+    "data-lake-nonprod",
+    stack_name="geospatial-data-lake-nonprod",
+    env={"region": "ap-southeast-2", "account": "632223577832"},
+)
+
+DataLakeStack(
+    app,
+    "data-lake-prod",
+    stack_name="geospatial-data-lake-prod",
+    env={"region": "ap-southeast-2", "account": "715898075157"},
+)
+
+app.synth()
