3470c1878915eeec2c3c2172c3ac1d300cbb31d7
Ivan Mincik <ivan.mincik@gmail.com>
2020-09-27 21:25:00 +0200

    ci: enable Pylint for infra directory

--- infra/data_stores/data_lake_stack.py
+++ infra/data_stores/data_lake_stack.py
@@ -1,8 +1,14 @@
+"""
+Data Lake AWS resources definitions.
+"""
+
 from aws_cdk import core
 
 
 class DataLakeStack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    """Data Lake stack definition."""
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:  # pylint: disable=W0622
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
--- sample.py
+++
@@ -1,6 +0,0 @@
-"""
-Test Python file. Remove this file once some real Python code will appear in
-this repository.
-"""
-
-print("Hello from Test Python file !")
