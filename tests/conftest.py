7de696951c71ffac5070967e5cf639de8ec4fc96
Ivan Mincik <ivan.mincik@gmail.com>
2020-09-29 20:27:22 +0200

    tests: use supported Localstack version

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ Pytest Localstack configuration.
 
 import logging
 
+import boto3
 import pytest_localstack
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
@@ -17,5 +18,5 @@ localstack = pytest_localstack.patch_fixture(  # pylint: disable=no-member
     scope="session",
     region_name="ap-southeast-2",
     autouse=True,
-    localstack_version="latest",
+    localstack_version="0.11.3",  # latest currently supported LS version
 )
