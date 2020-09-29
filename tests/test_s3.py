32a5f75c39d3eb60464c97f44d64485c33f7b7ba
Ivan Mincik <ivan.mincik@gmail.com>
2020-09-29 16:26:23 +0200

    tests: fix tests code formatting

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -4,10 +4,11 @@ Example S3 service test.
 
 import boto3
 
+
 def test_s3_bucket_creation():
     """Test S3 bucket creation."""
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource("s3")
     assert len(list(s3.buckets.all())) == 0
-    bucket = s3.Bucket('foobar')
+    bucket = s3.Bucket("foobar")
     bucket.create()
     assert len(list(s3.buckets.all())) == 1
