81906605faf28a6071539d9d4ad9c66d8c4a0613
Ivan Mincik <ivan.mincik@gmail.com>
2020-10-02 12:50:10 +0200

    ci: fix infra tests

--- infra/tests/test_stack.py
+++ infra/tests/test_stack.py
@@ -2,12 +2,18 @@
 AWS infrastructure tests.
 """
 
+import os
 import boto3
 
 CF = boto3.client("cloudformation")
 
 
+if "ENVIRONMENT_TYPE" in os.environ:
+    env = os.environ["ENVIRONMENT_TYPE"]
+else:
+    env = "dev"
+
 def test_stack_create_complete():
     """Test if CloudFormation stack is successfully created."""
-    response = CF.describe_stacks(StackName="geospatial-data-lake-dev")
+    response = CF.describe_stacks(StackName=f"geospatial-data-lake-{env}")
     assert response["Stacks"][0]["StackStatus"] == "CREATE_COMPLETE"
