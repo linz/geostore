332620c03f5dfc4c8b3fd40ac5a236ed3a1bf226
Ivan Mincik <ivan.mincik@gmail.com>
2020-10-04 21:43:09 +0200

    test(infra): splitting tests to separate files

--- infra/tests/test_infra.py
+++ infra/tests/test_infra.py
@@ -1,12 +1,12 @@
 """
-AWS infrastructure tests.
+Data Lake Storage Bucket tests.
 """
 
 import os
 
 import boto3
 
-CF = boto3.client("cloudformation")
+S3 = boto3.client("s3")
 
 
 if "ENVIRONMENT_TYPE" in os.environ:
@@ -15,7 +15,17 @@ else:
     ENV = "dev"
 
 
-def test_stack_create_complete():
-    """Test if CloudFormation stack is successfully created."""
-    response = CF.describe_stacks(StackName=f"geospatial-data-lake-{ENV}")
-    assert response["Stacks"][0]["StackStatus"] == "CREATE_COMPLETE"
+def test_storage_bucket_region():
+    """Test if Data Lake Storage S3 Bucket is created in correct region."""
+    response = S3.get_bucket_location(
+            Bucket=f"linz-geospatial-data-lake-{ENV}"
+    )
+    assert response["LocationConstraint"] == "ap-southeast-2"
+
+
+def test_storage_bucket_versioning():
+    """Test if Data Lake Storage S3 Bucket versioning is enabled."""
+    response = S3.get_bucket_versioning(
+            Bucket=f"linz-geospatial-data-lake-{ENV}"
+    )
+    assert response["Status"] == "Enabled"
---
+++ infra/tests/test_stack.py
@@ -0,0 +1,24 @@
+"""
+Data Lake stack tests.
+"""
+
+import os
+
+import boto3
+
+CF = boto3.client("cloudformation")
+
+
+if "ENVIRONMENT_TYPE" in os.environ:
+    ENV = os.environ["ENVIRONMENT_TYPE"]
+else:
+    ENV = "dev"
+
+
+def test_stack_create_complete():
+    """Test if CloudFormation stack is successfully created."""
+    response = CF.describe_stacks(StackName=f"geospatial-data-lake-{ENV}")
+    assert response["Stacks"][0]["StackStatus"] in (
+        "CREATE_COMPLETE",
+        "UPDATE_COMPLETE",
+    )
