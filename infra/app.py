22309db7cc1e1fa841fad5387d94d7980a1f20f7
Simon Planzer <splanzer@linz.govt.nz>
2020-10-05 07:30:27 +1300

    cd: AWS CD implementation
    closes #6

--- infra/app.py
+++ infra/app.py
@@ -4,23 +4,22 @@
 CDK application entry point file.
 """
 
+import os
+
 from aws_cdk import core
 from data_stores.data_lake_stack import DataLakeStack
 
+if "ENVIRONMENT_TYPE" in os.environ:
+    ENV = os.environ["ENVIRONMENT_TYPE"]
+else:
+    ENV = "dev"
+
 app = core.App()
-
 DataLakeStack(
     app,
-    "data-lake-nonprod",
-    stack_name="geospatial-data-lake-nonprod",
-    env={"region": "ap-southeast-2", "account": "632223577832"},
-)
-
-DataLakeStack(
-    app,
-    "data-lake-prod",
-    stack_name="geospatial-data-lake-prod",
-    env={"region": "ap-southeast-2", "account": "715898075157"},
+    "geospatial-data-lake",
+    stack_name=f"geospatial-data-lake-{ENV}",
+    env={"region": "ap-southeast-2"},
 )
 
 app.synth()
