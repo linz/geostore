03e3af697a8bc4becc29f218b2caf8cd192aafd3
Ivan Mincik <ivan.mincik@gmail.com>
2020-10-05 14:11:27 +0200

    test(infra): add more storage bucket tests

--- infra/tests/test_storage_bucket.py
+++ infra/tests/test_storage_bucket.py
@@ -15,7 +15,7 @@ else:
     ENV = "dev"
 
 
-def test_storage_bucket_region():
+def test_storage_bucket_location():
     """Test if Data Lake Storage S3 Bucket is created in correct region."""
     response = S3.get_bucket_location(Bucket=f"linz-geospatial-data-lake-{ENV}")
     assert response["LocationConstraint"] == "ap-southeast-2"
@@ -27,3 +27,14 @@ def test_storage_bucket_versioning():
         Bucket=f"linz-geospatial-data-lake-{ENV}"
     )
     assert response["Status"] == "Enabled"
+
+
+def test_storage_bucket_public_access_block():
+    """Test if Data Lake Storage S3 Bucket access is blocked for public."""
+    response = S3.get_public_access_block(
+        Bucket=f"linz-geospatial-data-lake-{ENV}"
+    )
+    assert response["PublicAccessBlockConfiguration"]["BlockPublicAcls"] is True
+    assert response["PublicAccessBlockConfiguration"]["IgnorePublicAcls"] is True
+    assert response["PublicAccessBlockConfiguration"]["BlockPublicPolicy"] is True
+    assert response["PublicAccessBlockConfiguration"]["RestrictPublicBuckets"] is True
