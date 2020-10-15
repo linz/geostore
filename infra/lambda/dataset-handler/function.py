0c059a4e53a1c82475462eb84bc96e9ef4a58bee
Ivan Mincik <ivan.mincik@gmail.com>
2020-10-15 13:09:38 +0200

    feat(infra): Dataset Space AWS infrastructure
    * add Dataset Space Lambda handler function
    * add 'datasets' DynamoDB table
    
    closes: #34

---
+++ infra/lambda/dataset-handler/function.py
@@ -0,0 +1,19 @@
+"""
+Dataset Space Lambda handler function.
+"""
+
+
+def lambda_handler(event, context):  # pylint:disable=unused-argument
+    """Main Lambda entry point."""
+
+    return True
+
+
+if __name__ == "__main__":
+    lambda_handler(
+        {
+            "x": "x",
+            "y": "y"
+        },
+        "context"
+    )
