975ea7610234e1bbe3172fcd9ac0ac3e8f60ac5d
Ivan Mincik <ivan.mincik@gmail.com>
2020-11-08 20:55:54 +0100

    tests: better integration testing of lambda function

--- infra/tests/test_lambda_functions.py
+++ infra/tests/test_lambda_functions.py
@@ -3,6 +3,7 @@ Lambda functions integration tests.
 """
 
 import json
+import uuid
 
 import boto3
 
@@ -10,10 +11,22 @@ LAMBDA = boto3.client("lambda")
 
 
 def test_should_launch_datasets_endpoint_lambda_function():
-    """Test if datasets endpoint lambda can be successfully launched."""
+    """
+    Test if datasets endpoint lambda can be successfully launched and has required permission to
+    create dataset in DB.
+    """
 
-    response = LAMBDA.invoke(FunctionName="datasets-endpoint", InvocationType="RequestResponse")
-    json_response = json.loads(response["Payload"].read().decode("utf-8"))
+    method = "POST"
+    body = {}
+    body["type"] = "RASTER"
+    body["title"] = f"Dataset {uuid.uuid1}.hex"
+    body["owning_group"] = "A_ABC_XYZ"
 
-    assert json_response["statusCode"] == 400
-    assert json_response["body"]["message"] == "Bad Request: 'httpMethod' is a required property."
+    resp = LAMBDA.invoke(
+        FunctionName="datasets-endpoint",
+        Payload=json.dumps({"httpMethod": method, "body": body}),
+        InvocationType="RequestResponse",
+    )
+    json_resp = json.loads(resp["Payload"].read().decode("utf-8"))
+
+    assert json_resp["statusCode"] == 201
