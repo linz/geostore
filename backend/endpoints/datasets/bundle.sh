01730f4e95b2905ac868c2fb1b09f515c10e9871
Ivan Mincik <ivan.mincik@gmail.com>
2020-11-08 21:41:15 +0100

    fix: fix bundle script

--- backend/endpoints/datasets/bundle.sh
+++ backend/endpoints/datasets/bundle.sh
@@ -1,9 +1,8 @@
-pip install --requirement=datasets/requirements.txt --target=/asset-output \
-&& \
-mkdir --parents /asset-output/endpoints/datasets \
-&& \
-touch {/asset-output/endpoints/__init__.py,/asset-output/endpoints/datasets/__init__.py} \
-&& \
-cp --archive --update --verbose datasets/*.py /asset-output/endpoints/datasets/ \
-&& \
+set -o errexit
+
+pip install --requirement=datasets/requirements.txt --target=/asset-output
+mkdir --parents /asset-output/endpoints/datasets
+touch /asset-output/endpoints/__init__.py
+touch /asset-output/endpoints/datasets/__init__.py
+cp --archive --update --verbose datasets/*.py /asset-output/endpoints/datasets/
 cp --archive --update --verbose utils.py /asset-output/endpoints/
