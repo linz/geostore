66744ec5530b377bc93c0a0b21ac7df6b103f42d
Victor Engmark <vengmark@linz.govt.nz>
2020-11-27 07:51:50 +1300

    refactor: Group together dataset utility functions

--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -4,12 +4,12 @@ from random import choice
 from ..endpoints.datasets.common import DATASET_TYPES
 
 
-def any_valid_dataset_type() -> str:
-    return choice(DATASET_TYPES)
-
-
 def random_string(length: int) -> str:
     return "".join(choice(string.printable) for _ in range(length))
+
+
+def any_valid_dataset_type() -> str:
+    return choice(DATASET_TYPES)
 
 
 def any_dataset_title() -> str:
