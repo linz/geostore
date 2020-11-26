e734c226c1a88549dc8cbb1b76d12cf3c2711baa
Victor Engmark <vengmark@linz.govt.nz>
2020-11-26 16:35:32 +1300

    test: Set random dataset title where irrelevant
    This marks the title as definitely irrelevant where it's supposed to be,
    and therefore implicitly as relevant everywhere else. It also ensures
    that we learn about any escaping issues immediately.

--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -1,3 +1,4 @@
+import string
 from random import choice
 
 from ..endpoints.datasets.common import DATASET_TYPES
@@ -5,3 +6,12 @@ from ..endpoints.datasets.common import DATASET_TYPES
 
 def any_valid_dataset_type() -> str:
     return choice(DATASET_TYPES)
+
+
+def random_string(length: int) -> str:
+    return "".join(choice(string.printable) for _ in range(length))
+
+
+def any_dataset_title() -> str:
+    """Arbitrary-length string"""
+    return random_string(20)
