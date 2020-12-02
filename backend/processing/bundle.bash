46d12a44bedf4d0c399076fed3011b654407cd4c
Ivan Mincik <ivan.mincik@gmail.com>
2020-12-02 12:56:23 +0100

    fix: fix bundling script

--- backend/processing/bundle.bash
+++ backend/processing/bundle.bash
@@ -13,6 +13,7 @@ fi
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 work_dir="$(mktemp --directory)"
+asset_root='/asset-output'
 
 if [ -f "${script_dir}/${1}/requirements.txt" ]; then
     python -m venv "${work_dir}/.venv"
@@ -20,9 +21,9 @@ if [ -f "${script_dir}/${1}/requirements.txt" ]; then
     . "${work_dir}/.venv/bin/activate"
     python -m pip install --upgrade pip
 
-    pip install --requirement="${script_dir}/${1}/requirements.txt" --target=/asset-output
+    pip install --requirement="${script_dir}/${1}/requirements.txt" --target=${asset_root}
 fi
 
-mkdir --parents /asset-output/processing/${1}
-cp --archive --update --verbose "${script_dir}/"*.py /asset-output/processing
-cp --archive --update --verbose "${script_dir}/${1}/"*.py /asset-output/processing/${1}
+mkdir --parents "${asset_root}/processing/${1}"
+cp --archive --update "${script_dir}/"*.py "${asset_root}/processing"
+cp --archive --update "${script_dir}/${1}/"*.py "${asset_root}/processing/${1}"
