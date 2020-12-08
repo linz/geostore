95739dea6276136cec36dd577cc3240cb1968a7c
Victor Engmark <vengmark@linz.govt.nz>
2020-12-09 08:13:53 +1300

    Create dataset instances per test (#99)

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,7 +3,6 @@ Pytest configuration file.
 """
 
 import logging
-from datetime import datetime, timedelta, timezone
 
 import pytest
 
@@ -14,42 +13,12 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture()
-def db_prepare():
-    """
-    Prepare DB with some dataset records and clean it up after test is
-    finished.
-    """
-
-    items = [
-        DatasetModel(
-            id="DATASET#111abc",
-            type="TYPE#RASTER",
-            title="Dataset ABC",
-            owning_group="A_ABC_XYZ",
-            created_at=datetime.now(timezone.utc) - timedelta(days=10),
-            updated_at=datetime.now(timezone.utc) - timedelta(days=1),
-        ),
-        DatasetModel(
-            id="DATASET#222xyz",
-            type="TYPE#RASTER",
-            title="Dataset XYZ",
-            owning_group="A_ABC_XYZ",
-            created_at=datetime.now(timezone.utc) - timedelta(days=100),
-            updated_at=datetime.now(timezone.utc) - timedelta(days=10),
-        ),
-    ]
-
-    logger.debug("Running DB Setup")
-
-    with DatasetModel.batch_write() as batch:
-        for item in items:
-            batch.save(item)
-
-    yield  # teardown
-
-    logger.debug("Running DB Teardown")
+def db_teardown():
+    logger.debug("Removing all dataset instances before test")
 
     for item in DatasetModel.scan():
         item.delete()
+
+    yield
 
     return True
