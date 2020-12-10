a16ea764c698e3a0c8548262bd3d5587ff852834
Victor Engmark <vengmark@linz.govt.nz>
2020-12-10 14:40:28 +1300

    feat: Pull out script to reset development environment
    Installs
    - Node.js packages,
    - Python packages and
    - Git hooks.
    
    Documents one-time setup instructions with links to upstream
    instructions.

---
+++ reset-dev-env.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+npm install
+
+poetry install --extras='cdk datasets-endpoint'
+
+# shellcheck source=/dev/null
+. .venv/bin/activate
+
+pre-commit install --hook-type=commit-msg --overwrite
+pre-commit install --hook-type=pre-commit --overwrite
