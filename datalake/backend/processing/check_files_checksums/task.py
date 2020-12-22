d1060a60887539c2bd38cdeecd76160c5661bdd0
Victor Engmark <vengmark@linz.govt.nz>
2020-12-22 15:12:13 +1300

    feat: Pass parameters through for validation

--- datalake/backend/processing/check_files_checksums/task.py
+++ datalake/backend/processing/check_files_checksums/task.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
-from typing import Callable
+import sys
+from argparse import ArgumentParser, Namespace
+from typing import TYPE_CHECKING
+from urllib.parse import urlparse
 
+import boto3
 from botocore.response import StreamingBody  # type: ignore[import]
 from multihash import FUNCS, decode  # type: ignore[import]
+
+if TYPE_CHECKING:
+    # When type checking we want to use the third party package's stub
+    from mypy_boto3_s3 import S3Client
+else:
+    # In production we want to avoid depending on a package which has no runtime impact
+    S3Client = object
+
+
+ARRAY_INDEX_VARIABLE_NAME = "AWS_BATCH_JOB_ARRAY_INDEX"
 
 CHUNK_SIZE = 1024
 
 
-def validate_url_multihash(
-    url: str, hex_multihash: str, url_reader: Callable[[str], StreamingBody]
-) -> bool:
-    url_stream = url_reader(url)
+def validate_url_multihash(url: str, hex_multihash: str, s3_client: S3Client) -> bool:
+    parsed_url = urlparse(url)
+    bucket = parsed_url.netloc
+    key = parsed_url.path.lstrip("/")
+    url_stream: StreamingBody = s3_client.get_object(Bucket=bucket, Key=key)["Body"]
     checksum_function_code = int(hex_multihash[:2], 16)
     checksum_function = FUNCS[checksum_function_code]
 
@@ -21,3 +36,23 @@ def validate_url_multihash(
     actual_digest: bytes = file_digest.digest()
     expected_digest: bytes = decode(bytes.fromhex(hex_multihash))
     return actual_digest == expected_digest
+
+
+def parse_arguments() -> Namespace:
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("--file-url", required=True)
+    argument_parser.add_argument("--hex-multihash", required=True)
+    return argument_parser.parse_args()
+
+
+def main() -> int:
+    arguments = parse_arguments()
+    s3_client = boto3.client("s3")
+
+    validate_url_multihash(arguments.file_url, arguments.hex_multihash, s3_client)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- datalake/backend/tests/test_check_files_checksums.py
+++ datalake/backend/tests/test_check_files_checksums.py
@@ -1,39 +1,46 @@
+import sys
 from io import BytesIO
-from typing import Callable, Mapping
+from unittest.mock import ANY, MagicMock, patch
 
 from botocore.auth import EMPTY_SHA256_HASH  # type: ignore[import]
 from botocore.response import StreamingBody  # type: ignore[import]
+from botocore.stub import Stubber  # type: ignore[import]
 from multihash import SHA2_256  # type: ignore[import]
+from mypy_boto3_s3 import S3Client
 
-from ..processing.check_files_checksums.task import validate_url_multihash
-from .utils import any_s3_url
+from ..processing.check_files_checksums.task import main, validate_url_multihash
+from .utils import any_hex_multihash, any_program_name, any_s3_url
 
 SHA256_BYTE_COUNT = len(EMPTY_SHA256_HASH) >> 1
 EMPTY_FILE_MULTIHASH = f"{SHA2_256:x}{SHA256_BYTE_COUNT:x}{EMPTY_SHA256_HASH}"
 
 
-def fake_url_reader(url_to_content: Mapping[str, bytes]) -> Callable[[str], StreamingBody]:
-    def read_url(url: str) -> StreamingBody:
-        content = url_to_content[url]
-        content_stream = BytesIO(content)
-        content_length = len(content)
-        return StreamingBody(content_stream, content_length)
-
-    return read_url
+def test_should_return_true_when_empty_file_checksum_matches(s3_client: S3Client) -> None:
+    s3_stubber = Stubber(s3_client)
+    s3_stubber.add_response("get_object", {"Body": StreamingBody(BytesIO(), 0)})
+    with s3_stubber:
+        assert validate_url_multihash(any_s3_url(), EMPTY_FILE_MULTIHASH, s3_client)
 
 
-def test_should_return_true_when_empty_file_checksum_matches() -> None:
-    url = any_s3_url()
-    url_reader = fake_url_reader({url: b""})
-    assert validate_url_multihash(url, EMPTY_FILE_MULTIHASH, url_reader)
-
-
-def test_should_return_false_when_checksum_does_not_match() -> None:
-    url = any_s3_url()
-    url_reader = fake_url_reader({url: b""})
+def test_should_return_false_when_checksum_does_not_match(s3_client: S3Client) -> None:
+    s3_stubber = Stubber(s3_client)
+    s3_stubber.add_response("get_object", {"Body": StreamingBody(BytesIO(), 0)})
     checksum = "0" * 64
     checksum_byte_count = 32
 
-    assert not validate_url_multihash(
-        url, f"{SHA2_256:x}{checksum_byte_count:x}{checksum}", url_reader
-    )
+    with s3_stubber:
+        assert not validate_url_multihash(
+            any_s3_url(), f"{SHA2_256:x}{checksum_byte_count:x}{checksum}", s3_client
+        )
+
+
+@patch("datalake.backend.processing.check_files_checksums.task.validate_url_multihash")
+def test_should_validate_given_url_and_checksum(validate_url_multihash_mock: MagicMock) -> None:
+    url = any_s3_url()
+    hex_multihash = any_hex_multihash()
+    sys.argv = [any_program_name(), f"--file-url={url}", f"--hex-multihash={hex_multihash}"]
+
+    with patch("boto3.client"):
+        assert main() == 0
+
+    validate_url_multihash_mock.assert_called_once_with(url, hex_multihash, ANY)
