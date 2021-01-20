0b3f60218c35b7b908615b2386e3d35c722b6b0f
Ivan Mincik <ivan.mincik@gmail.com>
2021-01-20 18:46:06 +0100

    refactor(infra): rename 'source_stack' to 'staging_stack' (closes #74)

--- datalake/staging_stack.py
+++ datalake/staging_stack.py
@@ -1,23 +1,23 @@
 from aws_cdk import aws_s3, core
 from aws_cdk.core import Tags
 
+STAGING_BUCKET_NAME = "linz-geospatial-data-lake-staging"
 
-class DatasetSourceStack(core.Stack):
+
+class StagingStack(core.Stack):
     def __init__(self, scope: core.Construct, stack_id: str, deploy_env, **kwargs) -> None:
         super().__init__(scope, stack_id, **kwargs)
 
         ############################################################################################
-        # ### DATASET SOURCE S3 BUCKET #############################################################
+        # ### DATASET STAGING S3 BUCKET ############################################################
         ############################################################################################
-        self.source_bucket = aws_s3.Bucket(
+        self.staging_bucket = aws_s3.Bucket(
             self,
-            "dataset-source-bucket",
-            bucket_name="{}-{}".format(
-                self.node.try_get_context("data-lake-dataset-source-bucket-name"), deploy_env
-            ),
+            "dataset-staging-bucket",
+            bucket_name=f"{STAGING_BUCKET_NAME}-{deploy_env}",
             access_control=aws_s3.BucketAccessControl.PRIVATE,
             block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
             versioned=True,
             removal_policy=core.RemovalPolicy.DESTROY,
         )
-        Tags.of(self.source_bucket).add("ApplicationLayer", "storage")
+        Tags.of(self.staging_bucket).add("ApplicationLayer", "storage")
