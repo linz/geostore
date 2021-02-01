9d25325b5a3172a82ed0ff160ebeac5f6665de3a
Victor Engmark <vengmark@linz.govt.nz>
2021-02-01 20:26:11 +0000

    feat: Save URL multihashes to DB (#179)

--- datalake/backend/tests/test_check_stac_metadata.py
+++ datalake/backend/tests/test_check_stac_metadata.py
@@ -1,23 +1,36 @@
 import sys
 from copy import deepcopy
-from io import StringIO
-from json import dump
-from typing import Any, Dict, Optional, TextIO
+from datetime import timedelta
+from hashlib import sha256, sha512
+from io import BytesIO, StringIO
+from json import dump, dumps
+from time import sleep
+from typing import Any, Dict, List, Optional, TextIO
 from unittest.mock import Mock, call, patch
 
 from jsonschema import ValidationError  # type: ignore[import]
-from pytest import raises
+from pytest import mark, raises
 
-from ..processing.check_stac_metadata.task import STACSchemaValidator, main
+from app import ENV
+
+from ..processing.check_stac_metadata.task import ProcessingAssetsModel, STACSchemaValidator, main
 from .utils import (
+    S3Object,
     any_dataset_description,
     any_dataset_id,
+    any_dataset_version_id,
+    any_error_message,
+    any_file_contents,
+    any_hex_multihash,
     any_https_url,
     any_past_datetime_string,
     any_program_name,
     any_s3_url,
     any_safe_filename,
+    any_stac_asset_name,
     any_stac_relation,
+    any_valid_dataset_type,
+    get_state_machine,
 )
 
 STAC_VERSION = "1.0.0-beta.2"
@@ -33,6 +46,8 @@ MINIMAL_VALID_STAC_OBJECT: Dict[str, Any] = {
         "temporal": {"interval": [[any_past_datetime_string(), None]]},
     },
 }
+
+BUCKET_NAME = f"linz-geospatial-data-lake-{ENV}"
 
 
 class MockJSONURLReader(Mock):
@@ -57,19 +72,20 @@ class MockJSONURLReader(Mock):
 
 def test_should_treat_minimal_stac_object_as_valid() -> None:
     url = any_s3_url()
-    url_reader = MockJSONURLReader({url: MINIMAL_VALID_STAC_OBJECT})
+    url_reader = MockJSONURLReader({url: deepcopy(MINIMAL_VALID_STAC_OBJECT)})
     STACSchemaValidator(url_reader).validate(url)
 
 
-def test_should_treat_any_missing_top_level_key_as_invalid() -> None:
+def test_should_treat_any_missing_top_level_key_as_invalid(subtests) -> None:
     url = any_s3_url()
     for key in MINIMAL_VALID_STAC_OBJECT:
-        stac_object = deepcopy(MINIMAL_VALID_STAC_OBJECT)
-        stac_object.pop(key)
+        with subtests.test(msg=key):
+            stac_object = deepcopy(MINIMAL_VALID_STAC_OBJECT)
+            stac_object.pop(key)
 
-        url_reader = MockJSONURLReader({url: stac_object})
-        with raises(ValidationError):
-            STACSchemaValidator(url_reader).validate(url)
+            url_reader = MockJSONURLReader({url: stac_object})
+            with raises(ValidationError):
+                STACSchemaValidator(url_reader).validate(url)
 
 
 def test_should_detect_invalid_datetime() -> None:
@@ -84,11 +100,30 @@ def test_should_detect_invalid_datetime() -> None:
 @patch("datalake.backend.processing.check_stac_metadata.task.STACSchemaValidator.validate")
 def test_should_validate_given_url(validate_url_mock) -> None:
     url = any_s3_url()
-    sys.argv = [any_program_name(), f"--metadata-url={url}"]
+    sys.argv = [
+        any_program_name(),
+        f"--metadata-url={url}",
+        f"--dataset-id={any_dataset_id()}",
+        f"--version-id={any_dataset_version_id()}",
+    ]
 
-    assert main() == 0
+    with patch("datalake.backend.processing.check_stac_metadata.task.ProcessingAssetsModel"):
+        assert main() == 0
 
     validate_url_mock.assert_called_once_with(url)
+
+
+@patch("datalake.backend.processing.check_stac_metadata.task.STACSchemaValidator.validate")
+def test_should_print_json_output_on_validation_failure(validate_url_mock) -> None:
+    validate_url_mock.side_effect = ValidationError(any_error_message())
+    sys.argv = [
+        any_program_name(),
+        f"--metadata-url={any_s3_url()}",
+        f"--dataset-id={any_dataset_id()}",
+        f"--version-id={any_dataset_version_id()}",
+    ]
+
+    assert main() == 1
 
 
 def test_should_validate_metadata_files_recursively() -> None:
@@ -98,7 +133,9 @@ def test_should_validate_metadata_files_recursively() -> None:
 
     stac_object = deepcopy(MINIMAL_VALID_STAC_OBJECT)
     stac_object["links"].append({"href": child_url, "rel": "child"})
-    url_reader = MockJSONURLReader({parent_url: stac_object, child_url: MINIMAL_VALID_STAC_OBJECT})
+    url_reader = MockJSONURLReader(
+        {parent_url: stac_object, child_url: deepcopy(MINIMAL_VALID_STAC_OBJECT)}
+    )
 
     STACSchemaValidator(url_reader).validate(parent_url)
 
@@ -165,3 +202,115 @@ def test_should_raise_exception_if_non_s3_url_is_passed() -> None:
 
     with raises(AssertionError, match=f"URL doesn't start with “s3://”: “{https_url}”"):
         STACSchemaValidator(url_reader).validate(https_url)
+
+
+def test_should_return_assets_from_validated_metadata_files() -> None:
+    url = any_s3_url()
+    stac_object = deepcopy(MINIMAL_VALID_STAC_OBJECT)
+    first_asset_url = any_s3_url()
+    first_asset_multihash = any_hex_multihash()
+    second_asset_url = any_s3_url()
+    second_asset_multihash = any_hex_multihash()
+    stac_object["assets"] = {
+        any_stac_asset_name(): {
+            "href": first_asset_url,
+            "checksum:multihash": first_asset_multihash,
+        },
+        any_stac_asset_name(): {
+            "href": second_asset_url,
+            "checksum:multihash": second_asset_multihash,
+        },
+    }
+    expected_assets = [
+        {"url": first_asset_url, "multihash": first_asset_multihash},
+        {"url": second_asset_url, "multihash": second_asset_multihash},
+    ]
+    url_reader = MockJSONURLReader({url: stac_object})
+
+    assets = STACSchemaValidator(url_reader).validate(url)
+
+    assert _sort_assets(assets) == _sort_assets(expected_assets)
+
+
+@mark.timeout(timedelta(minutes=20).total_seconds())
+@mark.infrastructure
+def test_should_insert_asset_urls_and_checksums_into_database(
+    step_functions_client, subtests
+) -> None:
+    # pylint: disable=too-many-locals
+    # Given a metadata file with two assets
+    first_asset_content = any_file_contents()
+    first_asset_multihash = sha256(first_asset_content).hexdigest()
+
+    second_asset_content = any_file_contents()
+    second_asset_multihash = sha512(second_asset_content).hexdigest()
+
+    dataset_id = any_dataset_id()
+    version_id = any_dataset_version_id()
+
+    with S3Object(
+        BytesIO(initial_bytes=first_asset_content), BUCKET_NAME, any_safe_filename()
+    ) as first_asset_s3_object, S3Object(
+        BytesIO(initial_bytes=second_asset_content), BUCKET_NAME, any_safe_filename()
+    ) as second_asset_s3_object:
+        expected_hash_key = f"DATASET#{dataset_id}#VERSION#{version_id}"
+        expected_items = [
+            ProcessingAssetsModel(
+                hash_key=expected_hash_key,
+                range_key="DATA_ITEM_INDEX#0",
+                url=first_asset_s3_object.url,
+                multihash=first_asset_multihash,
+            ),
+            ProcessingAssetsModel(
+                hash_key=expected_hash_key,
+                range_key="DATA_ITEM_INDEX#1",
+                url=second_asset_s3_object.url,
+                multihash=second_asset_multihash,
+            ),
+        ]
+
+        metadata_stac_object = deepcopy(MINIMAL_VALID_STAC_OBJECT)
+        metadata_stac_object["assets"] = {
+            any_stac_asset_name(): {
+                "href": first_asset_s3_object.url,
+                "checksum:multihash": first_asset_multihash,
+            },
+            any_stac_asset_name(): {
+                "href": second_asset_s3_object.url,
+                "checksum:multihash": second_asset_multihash,
+            },
+        }
+        metadata_content = dumps(metadata_stac_object).encode()
+        with S3Object(
+            BytesIO(initial_bytes=metadata_content), BUCKET_NAME, any_safe_filename()
+        ) as metadata_s3_object:
+            # When
+            datalake_state_machine = get_state_machine(step_functions_client)
+            state_machine_input = {
+                "dataset_id": dataset_id,
+                "version_id": version_id,
+                "type": any_valid_dataset_type(),
+                "metadata_url": metadata_s3_object.url,
+            }
+            execution_response = step_functions_client.start_execution(
+                stateMachineArn=datalake_state_machine["stateMachineArn"],
+                input=dumps(state_machine_input),
+            )
+
+            while (
+                execution := step_functions_client.describe_execution(
+                    executionArn=execution_response["executionArn"]
+                )
+            )["status"] == "RUNNING":
+                sleep(5)
+            assert execution["status"] == "SUCCEEDED", execution
+
+            # Then
+            actual_items = ProcessingAssetsModel.query(expected_hash_key)
+            for actual_item, expected_item in zip(actual_items, expected_items):
+                with subtests.test():
+                    assert actual_item.attribute_values == expected_item.attribute_values
+
+
+def _sort_assets(assets: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    return sorted(assets, key=lambda entry: entry["url"])
