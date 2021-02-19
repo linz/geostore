264a3311dfde1f14a39eb52fd6ed4c9e64b80a29
Victor Engmark <vengmark@linz.govt.nz>
2021-02-19 17:12:41 +1300

    test: Join tests for checksumming state machine branches
    By randomly using one or two assets we test both branches *eventually,*
    and since this is a particularly expensive test the tradeoff between
    learning about any issues early and a faster run is probably worth it at
    least in the short term.
    
    Had to make `S3Object` a proper `ContextManager` subclass after working
    around a `mypy` limitation related to grouping brackets
    <https://github.com/python/mypy/issues/10109#issuecomment-782108837>.

--- datalake/backend/tests/test_processing_stack.py
+++ datalake/backend/tests/test_processing_stack.py
@@ -1,9 +1,11 @@
 import logging
 import time
+from contextlib import nullcontext
 from copy import deepcopy
 from hashlib import sha256
 from io import BytesIO
 from json import dumps
+from typing import ContextManager, Optional
 
 from mypy_boto3_ssm import SSMClient
 from mypy_boto3_stepfunctions import SFNClient
@@ -13,10 +15,10 @@ from ..endpoints.dataset_versions import entrypoint
 from ..endpoints.dataset_versions.create import DATASET_VERSION_CREATION_STEP_FUNCTION
 from ..endpoints.utils import ResourceName
 from .utils import (
-    EMPTY_FILE_MULTIHASH,
     MINIMAL_VALID_STAC_OBJECT,
     Dataset,
     S3Object,
+    any_boolean,
     any_dataset_id,
     any_file_contents,
     any_lambda_context,
@@ -42,97 +44,53 @@ def test_should_create_state_machine_arn_parameter(ssm_client: SSMClient) -> Non
 
 @mark.timeout(1200)
 @mark.infrastructure
-def test_should_successfully_run_dataset_version_creation_process_with_a_single_asset(
+def test_should_successfully_run_dataset_version_creation_process(
     step_functions_client: SFNClient,
 ) -> None:
     key_prefix = any_safe_file_path()
-    metadata_object_key = "{}/{}.json".format(key_prefix, any_safe_filename())
     metadata = deepcopy(MINIMAL_VALID_STAC_OBJECT)
     s3_bucket_name = ResourceName.DATASET_STAGING_BUCKET_NAME.value
 
+    mandatory_asset_contents = any_file_contents()
+
+    # Test either branch of check_files_checksums_maybe_array randomly. Trade-off between cost of
+    # running test (~2m) and coverage.
+    optional_asset: ContextManager[Optional[S3Object]]
+    if any_boolean():
+        optional_asset_contents = any_file_contents()
+        optional_asset = S3Object(
+            file_object=BytesIO(initial_bytes=optional_asset_contents),
+            bucket_name=s3_bucket_name,
+            key=f"{key_prefix}/{any_safe_filename()}.txt",
+        )
+    else:
+        optional_asset = nullcontext()
+
     with S3Object(
-        BytesIO(initial_bytes=b""),
-        s3_bucket_name,
-        f"{key_prefix}/{any_safe_filename()}.tif",
-    ) as asset_s3_object:
+        file_object=BytesIO(initial_bytes=mandatory_asset_contents),
+        bucket_name=s3_bucket_name,
+        key=f"{key_prefix}/{any_safe_filename()}.txt",
+    ) as mandatory_asset_s3_object, optional_asset as optional_asset_s3_object:
         metadata["assets"] = {
             any_stac_asset_name(): {
-                "href": asset_s3_object.url,
-                "checksum:multihash": EMPTY_FILE_MULTIHASH,
-            },
-        }
-
-        with S3Object(
-            BytesIO(initial_bytes=dumps(metadata).encode()),
-            s3_bucket_name,
-            metadata_object_key,
-        ) as s3_metadata_file:
-            dataset_id = any_dataset_id()
-            dataset_type = any_valid_dataset_type()
-            with Dataset(dataset_id=dataset_id, dataset_type=dataset_type):
-
-                body = {}
-                body["id"] = dataset_id
-                body["metadata-url"] = s3_metadata_file.url
-                body["type"] = dataset_type
-
-                launch_response = entrypoint.lambda_handler(
-                    {"httpMethod": "POST", "body": body}, any_lambda_context()
-                )["body"]
-                logger.info("Executed State Machine: %s", launch_response)
-
-                # poll for State Machine State
-                while (
-                    execution := step_functions_client.describe_execution(
-                        executionArn=launch_response["execution_arn"]
-                    )
-                )["status"] == "RUNNING":
-                    logger.info("Polling for State Machine state %s", "." * 6)
-                    time.sleep(5)
-
-                assert execution["status"] == "SUCCEEDED", execution
-
-
-@mark.timeout(1200)
-@mark.infrastructure
-def test_should_successfully_run_dataset_version_creation_process_via_array_job(
-    step_functions_client: SFNClient,
-) -> None:
-
-    metadata_object_key = "{}/{}.json".format(any_safe_file_path(), any_safe_filename())
-    metadata = deepcopy(MINIMAL_VALID_STAC_OBJECT)
-    s3_bucket_name = ResourceName.DATASET_STAGING_BUCKET_NAME.value
-
-    first_asset_contents = any_file_contents()
-    second_asset_contents = any_file_contents()
-    with S3Object(
-        BytesIO(initial_bytes=first_asset_contents),
-        s3_bucket_name,
-        f"{any_safe_file_path()}/{any_safe_filename()}.txt",
-    ) as first_asset_s3_object, S3Object(
-        BytesIO(initial_bytes=second_asset_contents),
-        s3_bucket_name,
-        f"{any_safe_file_path()}/{any_safe_filename()}.txt",
-    ) as second_asset_s3_object:
-        metadata["assets"] = {
-            any_stac_asset_name(): {
-                "href": first_asset_s3_object.url,
+                "href": mandatory_asset_s3_object.url,
                 "checksum:multihash": sha256_hex_digest_to_multihash(
-                    sha256(first_asset_contents).hexdigest()
-                ),
-            },
-            any_stac_asset_name(): {
-                "href": second_asset_s3_object.url,
-                "checksum:multihash": sha256_hex_digest_to_multihash(
-                    sha256(second_asset_contents).hexdigest()
+                    sha256(mandatory_asset_contents).hexdigest()
                 ),
             },
         }
+        if optional_asset_s3_object is not None:
+            metadata["assets"][any_stac_asset_name()] = {
+                "href": optional_asset_s3_object.url,
+                "checksum:multihash": sha256_hex_digest_to_multihash(
+                    sha256(optional_asset_contents).hexdigest()
+                ),
+            }
 
         with S3Object(
-            BytesIO(initial_bytes=dumps(metadata).encode()),
-            s3_bucket_name,
-            metadata_object_key,
+            file_object=BytesIO(initial_bytes=dumps(metadata).encode()),
+            bucket_name=s3_bucket_name,
+            key=("{}/{}.json".format(key_prefix, any_safe_filename())),
         ) as s3_metadata_file:
             dataset_id = any_dataset_id()
             dataset_type = any_valid_dataset_type()
