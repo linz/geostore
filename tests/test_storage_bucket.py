35cc2a040c0454310c0d272d6c786544c212f935
Victor Engmark <vengmark@linz.govt.nz>
2021-03-10 09:34:23 +1300

    fix: Use "should_" test name prefix in more tests (#364)
    https://github.com/linz/geospatial-data-lake/pull/364

--- tests/test_storage_bucket.py
+++ tests/test_storage_bucket.py
@@ -11,21 +11,21 @@ from backend.resources import ResourceName
 
 
 @mark.infrastructure
-def test_storage_bucket_location(s3_client: S3Client) -> None:
+def should_create_storage_bucket_location_constraint(s3_client: S3Client) -> None:
     """Test if Data Lake Storage S3 Bucket is created in correct region."""
     response = s3_client.get_bucket_location(Bucket=ResourceName.STORAGE_BUCKET_NAME.value)
     assert response["LocationConstraint"] == "ap-southeast-2"
 
 
 @mark.infrastructure
-def test_storage_bucket_versioning(s3_client: S3Client) -> None:
+def should_enable_storage_bucket_versioning(s3_client: S3Client) -> None:
     """Test if Data Lake Storage S3 Bucket versioning is enabled."""
     response = s3_client.get_bucket_versioning(Bucket=ResourceName.STORAGE_BUCKET_NAME.value)
     assert response["Status"] == "Enabled"
 
 
 @mark.infrastructure
-def test_storage_bucket_public_access_block(s3_client: S3Client) -> None:
+def should_create_storage_bucket_public_access_block(s3_client: S3Client) -> None:
     """Test if Data Lake Storage S3 Bucket access is blocked for public."""
     response = s3_client.get_public_access_block(Bucket=ResourceName.STORAGE_BUCKET_NAME.value)
     assert response["PublicAccessBlockConfiguration"]["BlockPublicAcls"] is True
