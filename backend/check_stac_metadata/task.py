824c3772d5cc29d6498fa1ffcc26b0b4bdab64a8
Victor Engmark <vengmark@linz.govt.nz>
2021-03-10 13:37:29 +1300

    refactor: Pull out initialisation code

--- backend/check_stac_metadata/task.py
+++ backend/check_stac_metadata/task.py
@@ -11,40 +11,39 @@ from jsonschema import ValidationError  # type: ignore[import]
 from ..log import set_up_logging
 from .utils import STACDatasetValidator, ValidationResultFactory, parse_arguments
 
+LOGGER = set_up_logging(__name__)
+S3_CLIENT = boto3.client("s3")
+
 
 def s3_url_reader() -> Callable[[str], StreamingBody]:
-    client = boto3.client("s3")
-
     def read(href: str) -> StreamingBody:
         parse_result = urlparse(href, allow_fragments=False)
         bucket_name = parse_result.netloc
         key = parse_result.path[1:]
-        response = client.get_object(Bucket=bucket_name, Key=key)
+        response = S3_CLIENT.get_object(Bucket=bucket_name, Key=key)
         return response["Body"]
 
     return read
 
 
 def main() -> int:
-    logger = set_up_logging(__name__)
-
-    arguments = parse_arguments(logger)
+    arguments = parse_arguments(LOGGER)
 
     url_reader = s3_url_reader()
     hash_key = f"DATASET#{arguments.dataset_id}#VERSION#{arguments.version_id}"
     validation_result_factory = ValidationResultFactory(hash_key)
-    validator = STACDatasetValidator(url_reader, logger, validation_result_factory)
+    validator = STACDatasetValidator(url_reader, LOGGER, validation_result_factory)
 
     try:
         validator.validate(arguments.metadata_url)
 
     except (AssertionError, ValidationError) as error:
-        logger.error(dumps({"success": False, "message": str(error)}))
+        LOGGER.error(dumps({"success": False, "message": str(error)}))
         return 1
 
     validator.save(hash_key)
 
-    logger.info(dumps({"success": True, "message": ""}))
+    LOGGER.info(dumps({"success": True, "message": ""}))
     return 0
 
 
