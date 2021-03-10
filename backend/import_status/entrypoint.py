f07eebd8b6a2883f9448ae9a30b1034c2ac94d8f
Mitchell Paff <mpaff@linz.govt.nz>
2021-03-10 16:37:55 +1300

    refactor: change entrypoint to use new utility method to validate input

--- backend/import_status/entrypoint.py
+++ backend/import_status/entrypoint.py
@@ -3,19 +3,8 @@ Dataset-versions endpoint Lambda function.
 """
 from typing import Callable, Mapping
 
-from jsonschema import ValidationError, validate  # type: ignore[import]
-
-from ..api_responses import JsonObject, error_response
+from ..api_responses import JsonObject, handle_request
 from .get import get_import_status
-
-REQUEST_SCHEMA = {
-    "type": "object",
-    "properties": {
-        "httpMethod": {"type": "string", "enum": ["GET"]},
-        "body": {"type": "object"},
-    },
-    "required": ["httpMethod", "body"],
-}
 
 REQUEST_HANDLERS: Mapping[str, Callable[[JsonObject], JsonObject]] = {
     "GET": get_import_status,
@@ -23,13 +12,4 @@ REQUEST_HANDLERS: Mapping[str, Callable[[JsonObject], JsonObject]] = {
 
 
 def lambda_handler(event: JsonObject, _context: bytes) -> JsonObject:
-    """Main Lambda entry point."""
-
-    # request validation
-    try:
-        validate(event, REQUEST_SCHEMA)
-    except ValidationError as err:
-        return error_response(400, err.message)
-
-    method = event["httpMethod"]
-    return REQUEST_HANDLERS[method](event)
+    return handle_request(event, REQUEST_HANDLERS)
