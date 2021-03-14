ab01ed6a7030cb69dca28276bf6465722db21c3a
Victor Engmark <vengmark@linz.govt.nz>
2021-03-15 11:03:22 +1300

    test: Add guard for not copying dataset files on failure

--- tests/test_processing_stack.py
+++ tests/test_processing_stack.py
@@ -11,7 +11,7 @@ from mypy_boto3_s3control import S3ControlClient
 from mypy_boto3_ssm import SSMClient
 from mypy_boto3_stepfunctions import SFNClient
 from mypy_boto3_sts import STSClient
-from pytest import mark
+from pytest import mark, raises
 from pytest_subtests import SubTests  # type: ignore[import]
 
 from backend.dataset_versions.create import DATASET_VERSION_CREATION_STEP_FUNCTION
@@ -31,6 +31,7 @@ from .general_generators import any_file_contents, any_safe_file_path, any_safe_
 from .stac_generators import (
     any_asset_name,
     any_dataset_id,
+    any_hex_multihash,
     any_valid_dataset_type,
     sha256_hex_digest_to_multihash,
 )
@@ -334,3 +335,80 @@ def should_successfully_run_dataset_version_creation_process_with_single_asset(
                     )
                     status_json_resp = json.load(status_resp["Payload"])
                     assert status_json_resp == expected_response
+
+
+@mark.infrastructure
+def should_not_copy_files_when_there_is_a_checksum_mismatch(
+    lambda_client: LambdaClient,
+    s3_client: S3Client,
+    step_functions_client: SFNClient,
+    subtests: SubTests,
+) -> None:
+    # Given an asset with an invalid checksum
+    dataset_id = any_dataset_id()
+    dataset_type = any_valid_dataset_type()
+
+    key_prefix = any_safe_file_path()
+
+    with S3Object(
+        file_object=BytesIO(),
+        bucket_name=ResourceName.DATASET_STAGING_BUCKET_NAME.value,
+        key=f"{key_prefix}/{any_safe_filename()}.txt",
+    ) as asset_s3_object:
+        metadata = {
+            **deepcopy(MINIMAL_VALID_STAC_OBJECT),
+            "item_assets": {
+                any_asset_name(): {
+                    "href": asset_s3_object.url,
+                    "checksum:multihash": any_hex_multihash(),
+                },
+            },
+        }
+
+        with S3Object(
+            file_object=json_dict_to_file_object(metadata),
+            bucket_name=ResourceName.DATASET_STAGING_BUCKET_NAME.value,
+            key=f"{key_prefix}/{any_safe_filename()}.json",
+        ) as s3_metadata_file, Dataset(dataset_id=dataset_id, dataset_type=dataset_type):
+
+            # When creating a dataset version
+            dataset_version_creation_response = lambda_client.invoke(
+                FunctionName=ResourceName.DATASET_VERSIONS_ENDPOINT_FUNCTION_NAME.value,
+                Payload=json.dumps(
+                    {
+                        "httpMethod": "POST",
+                        "body": {
+                            "id": dataset_id,
+                            "metadata-url": s3_metadata_file.url,
+                            "type": dataset_type,
+                        },
+                    }
+                ).encode(),
+                InvocationType="RequestResponse",
+            )
+
+            response_payload = json.load(dataset_version_creation_response["Payload"])
+            with subtests.test(msg="Dataset Versions endpoint status code"):
+                assert response_payload.get("statusCode") == 201, response_payload
+
+            with subtests.test(msg="Step function result"):
+                # Then poll for State Machine State
+                state_machine_arn = response_payload["body"]["execution_arn"]
+                while (
+                    execution := step_functions_client.describe_execution(
+                        executionArn=state_machine_arn
+                    )
+                )["status"] == "RUNNING":
+                    logger.info("Polling for State Machine %s state", state_machine_arn)
+                    time.sleep(5)
+
+                assert execution["status"] == "SUCCEEDED", execution
+
+    # Then the files should not be copied
+    for key in [s3_metadata_file.key, asset_s3_object.key]:
+        with raises(AssertionError):
+            delete_s3_key(
+                ResourceName.STORAGE_BUCKET_NAME.value,
+                f"{dataset_id}/{response_payload['body']['dataset_version']}/{key}",
+                s3_client,
+            )
