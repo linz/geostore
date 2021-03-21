711026b6cd11202d344bfbe46a0720b93440a2b0
Victor Engmark <vengmark@linz.govt.nz>
2021-03-22 11:17:26 +1300

    feat: Log missing parameters
    The original error message doesn't contain the parameter name.

---
+++ tests/test_parameter_store.py
@@ -0,0 +1,23 @@
+from json import dumps
+from logging import getLogger
+from unittest.mock import MagicMock, patch
+
+from pytest import mark, raises
+
+from backend import parameter_store
+from backend.parameter_store import SSM_CLIENT, ParameterName, get_param
+
+
+@mark.infrastructure
+@patch(f"{parameter_store.__name__}.{ParameterName.__name__}")
+def should_log_missing_parameter_name(parameter_name_mock: MagicMock) -> None:
+    logger = getLogger(parameter_store.__name__)
+    parameter_name = "invalid"
+    parameter_name_mock.INVALID.value = parameter_name
+
+    with patch.object(logger, "error") as logger_mock, raises(
+        SSM_CLIENT.exceptions.ParameterNotFound
+    ):
+        get_param(parameter_name_mock.INVALID)
+
+        logger_mock.assert_any_call(dumps({"error": f"Parameter not found: “{parameter_name}”"}))
