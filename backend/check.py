2128cd23aae38d240d259baa3543a86390f8965d
Mitchell Paff <77649372+MitchellPaff@users.noreply.github.com>
2021-03-25 11:03:22 +1300

    feat: Add user validation for non S3 URL and multiple directories (#448)
    * feat: added user validation for non s3 url and multiple directories
    
    * fix: addressed PR comments

--- backend/check.py
+++ backend/check.py
@@ -5,3 +5,5 @@ class Check(Enum):
     CHECKSUM = "checksum"
     JSON_SCHEMA = "JSON schema"
     STAGING_ACCESS = "staging bucket access"
+    MULTIPLE_DIRECTORIES = "multiple directories"
+    NON_S3_URL = "not an s3 url"
