753274500fa3be1e7df30ba717c912e0b0b28f4a
Mitchell Paff <mpaff@linz.govt.nz>
2021-04-06 16:19:04 +1200

    fix: update stac version

--- tests/aws_utils.py
+++ tests/aws_utils.py
@@ -40,8 +40,6 @@ from .stac_generators import (
     any_valid_dataset_type,
 )
 
-STAC_VERSION = "1.0.0-rc.2"
-
 SHA256_BYTE_COUNT = len(EMPTY_SHA256_HASH) >> 1
 EMPTY_FILE_MULTIHASH = f"{SHA2_256:x}{SHA256_BYTE_COUNT:x}{EMPTY_SHA256_HASH}"
 
