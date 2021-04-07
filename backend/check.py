046dd7d44aed022296873db027bfd4fce59e10b8
Victor Engmark <vengmark@linz.govt.nz>
2021-04-07 16:55:22 +1200

    chore: Remove unused enum value

--- backend/check.py
+++ backend/check.py
@@ -5,5 +5,4 @@ class Check(Enum):
     CHECKSUM = "checksum"
     JSON_SCHEMA = "JSON schema"
     STAGING_ACCESS = "staging bucket access"
-    MULTIPLE_DIRECTORIES = "multiple directories"
     NON_S3_URL = "not an s3 url"
