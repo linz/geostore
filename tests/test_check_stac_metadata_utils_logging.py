ab3400377a21d31ef66939540eebe52f253dbd04
Mitchell Paff <77649372+MitchellPaff@users.noreply.github.com>
2021-04-08 16:55:11 +1200

    refactor: Modified stac validation task to not crash pipeline (#513)
    * refactor: Modified stac validation task to not crash pipeline
    
    * fix: added tests for logging
    
    * fix: rebased and added logging test for incorrect json
    
    * refactor: address PR comments
    
    * fix: fix no return from main in tests
    
    * fix: fixed indentation of test

--- tests/test_check_stac_metadata_utils_logging.py
+++ tests/test_check_stac_metadata_utils_logging.py
@@ -1,13 +1,22 @@
 import logging
 import sys
 from copy import deepcopy
-from json import dumps
+from io import StringIO
+from json import JSONDecodeError, dumps
 from unittest.mock import MagicMock, patch
 
-from backend.check_stac_metadata.utils import STACDatasetValidator, parse_arguments
+from botocore.exceptions import ClientError  # type: ignore[import]
+from jsonschema import ValidationError  # type: ignore[import]
+
+from backend.check_stac_metadata.utils import S3_URL_PREFIX, STACDatasetValidator, parse_arguments
 
 from .aws_utils import MockJSONURLReader, MockValidationResultFactory, any_s3_url
-from .general_generators import any_program_name, any_safe_filename
+from .general_generators import (
+    any_error_message,
+    any_https_url,
+    any_program_name,
+    any_safe_filename,
+)
 from .stac_generators import (
     any_asset_name,
     any_dataset_id,
@@ -62,5 +71,83 @@ def should_log_assets() -> None:
         "backend.check_stac_metadata.utils.processing_assets_model_with_meta"
     ):
         STACDatasetValidator(url_reader, MockValidationResultFactory()).validate(metadata_url)
+
+        logger_mock.assert_any_call(expected_message)
+
+
+def should_log_non_s3_url_prefix_validation() -> None:
+    metadata_url = any_https_url()
+    hash_key = f"DATASET#{any_dataset_id()}#VERSION#{any_dataset_version_id()}"
+    url_reader = MockJSONURLReader({metadata_url: MINIMAL_VALID_STAC_COLLECTION_OBJECT})
+    expected_message = dumps(
+        {"success": False, "message": f"URL doesn't start with “{S3_URL_PREFIX}”: “{metadata_url}”"}
+    )
+
+    with patch.object(LOGGER, "error") as logger_mock, patch(
+        "backend.check_stac_metadata.utils.processing_assets_model_with_meta"
+    ):
+        STACDatasetValidator(url_reader, MockValidationResultFactory()).run(metadata_url, hash_key)
+
+        logger_mock.assert_any_call(expected_message)
+
+
+@patch("backend.check_stac_metadata.utils.STACDatasetValidator.validate")
+def should_log_staging_access_validation(validate_mock: MagicMock) -> None:
+    metadata_url = any_s3_url()
+    hash_key = f"DATASET#{any_dataset_id()}#VERSION#{any_dataset_version_id()}"
+
+    expected_error = ClientError(
+        {"Error": {"Code": "TEST", "Message": "TEST"}}, operation_name="get_object"
+    )
+    validate_mock.side_effect = expected_error
+
+    url_reader = MockJSONURLReader({metadata_url: MINIMAL_VALID_STAC_COLLECTION_OBJECT})
+
+    expected_message = dumps({"success": False, "message": str(expected_error)})
+
+    with patch.object(LOGGER, "error") as logger_mock, patch(
+        "backend.check_stac_metadata.utils.processing_assets_model_with_meta"
+    ):
+        STACDatasetValidator(url_reader, MockValidationResultFactory()).run(metadata_url, hash_key)
+
+        logger_mock.assert_any_call(expected_message)
+
+
+@patch("backend.check_stac_metadata.utils.STACDatasetValidator.validate")
+def should_log_schema_mismatch_validation(validate_mock: MagicMock) -> None:
+    metadata_url = any_s3_url()
+    hash_key = f"DATASET#{any_dataset_id()}#VERSION#{any_dataset_version_id()}"
+
+    expected_error = ValidationError(any_error_message())
+    validate_mock.side_effect = expected_error
+
+    url_reader = MockJSONURLReader({metadata_url: MINIMAL_VALID_STAC_COLLECTION_OBJECT})
+
+    expected_message = dumps({"success": False, "message": expected_error.message})
+
+    with patch.object(LOGGER, "error") as logger_mock, patch(
+        "backend.check_stac_metadata.utils.processing_assets_model_with_meta"
+    ):
+        STACDatasetValidator(url_reader, MockValidationResultFactory()).run(metadata_url, hash_key)
+
+        logger_mock.assert_any_call(expected_message)
+
+
+@patch("backend.check_stac_metadata.utils.STACDatasetValidator.validate")
+def should_log_json_parse_validation(validate_mock: MagicMock) -> None:
+    metadata_url = any_s3_url()
+    hash_key = f"DATASET#{any_dataset_id()}#VERSION#{any_dataset_version_id()}"
+
+    url_reader = MockJSONURLReader({metadata_url: StringIO(initial_value="{")})
+
+    expected_error = JSONDecodeError(any_error_message(), "", 0)
+    validate_mock.side_effect = expected_error
+
+    expected_message = dumps({"success": False, "message": str(expected_error)})
+
+    with patch.object(LOGGER, "error") as logger_mock, patch(
+        "backend.check_stac_metadata.utils.processing_assets_model_with_meta"
+    ):
+        STACDatasetValidator(url_reader, MockValidationResultFactory()).run(metadata_url, hash_key)
 
         logger_mock.assert_any_call(expected_message)
