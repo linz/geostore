7f15c16a4a0937dc8041c3738173e71ede1cd9cf
Victor Engmark <vengmark@linz.govt.nz>
2021-04-08 00:16:54 +0000

    chore: Remove unused enum value (#512)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- backend/check.py
+++ backend/check.py
@@ -6,5 +6,4 @@ class Check(Enum):
     DUPLICATE_OBJECT_KEY = "duplicate asset name"
     JSON_SCHEMA = "JSON schema"
     STAGING_ACCESS = "staging bucket access"
-    MULTIPLE_DIRECTORIES = "multiple directories"
     NON_S3_URL = "not an s3 url"
