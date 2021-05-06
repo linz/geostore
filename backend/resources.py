0199330cbfde26d16f310ff2734009de0ac72808
Mitchell Paff <mpaff@linz.govt.nz>
2021-05-07 11:26:17 +1200

    refactor: update name of buckets

--- backend/resources.py
+++ backend/resources.py
@@ -9,5 +9,5 @@ class ResourceName(Enum):
     IMPORT_STATUS_ENDPOINT_FUNCTION_NAME = f"{ENV}-import-status"
     READ_ONLY_ROLE_NAME = f"{ENV}-data-lake-readonly"
     USERS_ROLE_NAME = f"{ENV}-data-lake-users"
-    STORAGE_BUCKET_NAME = f"{ENV}-storage"
-    STAGING_BUCKET_NAME = f"{ENV}-staging"
+    STORAGE_BUCKET_NAME = f"{ENV}-geospatial-data-lake"
+    STAGING_BUCKET_NAME = f"{ENV}-geospatial-data-lake-staging"
