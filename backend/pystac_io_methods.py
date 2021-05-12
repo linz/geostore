ce9ed76ab7e9433c129cf3f7e5703325a7fcc06f
Mitchell Paff <mpaff@linz.govt.nz>
2021-05-12 16:31:24 +1200

    refactor: further pr comments

--- backend/pystac_io_methods.py
+++ backend/pystac_io_methods.py
@@ -1,4 +1,4 @@
-from typing import IO, Any, Union
+from typing import IO, Union
 from urllib.parse import urlparse
 
 import boto3
@@ -7,22 +7,24 @@ from pystac import STAC_IO  # type: ignore[import]
 S3_CLIENT = boto3.client("s3")
 
 
-def read_method(uri: str) -> Any:
+def read_method(uri: str) -> str:
     parsed = urlparse(uri)
+    result: str = STAC_IO.default_read_text_method(uri)
+
     if parsed.scheme == "s3":
         bucket = parsed.netloc
         key = parsed.path[1:]
         obj = S3_CLIENT.get_object(Bucket=bucket, Key=key)
-        return obj["Body"].read().decode("utf-8")
+        result = obj["Body"].read().decode("utf-8")
 
-    return STAC_IO.default_read_text_method(uri)
+    return result
 
 
-def write_method(uri: str, txt: Union[bytes, IO[bytes]]) -> None:
+def write_method(uri: str, body: Union[bytes, IO[bytes]]) -> None:
     parsed = urlparse(uri)
     if parsed.scheme == "s3":
         bucket = parsed.netloc
         key = parsed.path[1:]
-        S3_CLIENT.put_object(Bucket=bucket, Key=key, Body=txt)
+        S3_CLIENT.put_object(Bucket=bucket, Key=key, Body=body)
     else:
-        STAC_IO.default_write_text_method(uri, txt)
+        STAC_IO.default_write_text_method(uri, body)
