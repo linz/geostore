39759fde8a7202cc9641ca287366636b84d7bb46
Victor Engmark <vengmark@linz.govt.nz>
2021-05-19 10:11:49 +1200

    tests: Fix unreachable assertions

--- tests/test_check_files_checksums_logging.py
+++ tests/test_check_files_checksums_logging.py
@@ -19,50 +19,44 @@ from backend.processing_assets_model import ProcessingAssetType, ProcessingAsset
 from .general_generators import any_program_name
 from .stac_generators import any_dataset_id, any_dataset_version_id
 
+LOGGER = logging.getLogger("backend.check_files_checksums.task")
 
-class TestLogging:
-    logger: logging.Logger
 
-    @classmethod
-    def setup_class(cls) -> None:
-        cls.logger = logging.getLogger("backend.check_files_checksums.task")
+@mark.infrastructure
+def should_log_missing_item(subtests: SubTests) -> None:
+    # Given
+    dataset_id = any_dataset_id()
+    version_id = any_dataset_version_id()
+    index = 0
+    expected_log = dumps(
+        {
+            SUCCESS_KEY: False,
+            ERROR_KEY: {MESSAGE_KEY: ProcessingAssetsModelBase.DoesNotExist.msg},
+            "parameters": {
+                "hash_key": (
+                    f"{DATASET_ID_PREFIX}{dataset_id}"
+                    f"{DB_KEY_SEPARATOR}{VERSION_ID_PREFIX}{version_id}"
+                ),
+                "range_key": f"{ProcessingAssetType.DATA.value}{DB_KEY_SEPARATOR}{index}",
+            },
+        }
+    )
 
-    @mark.infrastructure
-    def should_log_missing_item(self, subtests: SubTests) -> None:
-        # Given
-        dataset_id = any_dataset_id()
-        version_id = any_dataset_version_id()
-        index = 0
-        expected_log = dumps(
-            {
-                SUCCESS_KEY: False,
-                ERROR_KEY: {MESSAGE_KEY: ProcessingAssetsModelBase.DoesNotExist.msg},
-                "parameters": {
-                    "hash_key": (
-                        f"{DATASET_ID_PREFIX}{dataset_id}"
-                        f"{DB_KEY_SEPARATOR}{VERSION_ID_PREFIX}{version_id}"
-                    ),
-                    "range_key": f"{ProcessingAssetType.DATA.value}{DB_KEY_SEPARATOR}{index}",
-                },
-            }
-        )
+    sys.argv = [
+        any_program_name(),
+        f"--dataset-id={dataset_id}",
+        f"--version-id={version_id}",
+        f"--assets-table-name={get_param(ParameterName.PROCESSING_ASSETS_TABLE_NAME)}",
+        f"--results-table-name={get_param(ParameterName.STORAGE_VALIDATION_RESULTS_TABLE_NAME)}",
+        f"--first-item={index}",
+    ]
 
-        sys.argv = [
-            any_program_name(),
-            f"--dataset-id={dataset_id}",
-            f"--version-id={version_id}",
-            f"--assets-table-name={get_param(ParameterName.PROCESSING_ASSETS_TABLE_NAME)}",
-            (
-                "--results-table-name"
-                f"={get_param(ParameterName.STORAGE_VALIDATION_RESULTS_TABLE_NAME)}"
-            ),
-            f"--first-item={index}",
-        ]
-
-        # When/Then
-        with patch.object(self.logger, "error") as logger_mock, subtests.test(
-            msg="Return code"
-        ), raises(DoesNotExist), patch.dict(environ, {ARRAY_INDEX_VARIABLE_NAME: "0"}):
+    # When/Then
+    with patch.object(LOGGER, "error") as logger_mock, patch.dict(
+        environ, {ARRAY_INDEX_VARIABLE_NAME: "0"}
+    ):
+        with subtests.test(msg="Return code"), raises(DoesNotExist):
             main()
-            with subtests.test(msg="Log message"):
-                logger_mock.assert_any_call(expected_log)
+
+        with subtests.test(msg="Log message"):
+            logger_mock.assert_any_call(expected_log)
--- tests/test_parameter_store.py
+++ tests/test_parameter_store.py
@@ -16,9 +16,8 @@ def should_log_missing_parameter_name(parameter_name_mock: MagicMock) -> None:
     parameter_name = "invalid"
     parameter_name_mock.INVALID.value = parameter_name
 
-    with patch.object(logger, "error") as logger_mock, raises(
-        SSM_CLIENT.exceptions.ParameterNotFound
-    ):
-        get_param(parameter_name_mock.INVALID)
+    with patch.object(logger, "error") as logger_mock:
+        with raises(SSM_CLIENT.exceptions.ParameterNotFound):
+            get_param(parameter_name_mock.INVALID)
 
         logger_mock.assert_any_call(dumps({ERROR_KEY: f"Parameter not found: “{parameter_name}”"}))
