76786995bb60e6cc272f79033f7c69cb1bba22d1
Victor Engmark <vengmark@linz.govt.nz>
2021-05-19 02:29:46 +0000

    tests: Prep for 100% coverage (#729)
    * tests: Use multi-chunk file for checksum test
    
    Makes sure we test the chunk handling.
    
    * tests: Verify check STAC metadata JSON validation handling
    
    * chore: Remove unused function
    
    * tests: Exclude otherwise tested lines from coverage
    
    * tests: Fix unreachable assertions
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- tests/test_check_stac_metadata_task_logging.py
+++ tests/test_check_stac_metadata_task_logging.py
@@ -1,29 +1,56 @@
 import logging
+from copy import deepcopy
 from json import dumps
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
+
+from jsonschema import ValidationError  # type: ignore[import]
+from pytest_subtests import SubTests  # type: ignore[import]
 
 from backend.check_stac_metadata.task import lambda_handler
+from backend.error_response_keys import ERROR_KEY, ERROR_MESSAGE_KEY
 from backend.step_function import DATASET_ID_KEY, METADATA_URL_KEY, VERSION_ID_KEY
 from tests.aws_utils import any_lambda_context, any_s3_url
+from tests.general_generators import any_error_message
 from tests.stac_generators import any_dataset_id, any_dataset_version_id
+
+MINIMAL_PAYLOAD = {
+    DATASET_ID_KEY: any_dataset_id(),
+    VERSION_ID_KEY: any_dataset_version_id(),
+    METADATA_URL_KEY: any_s3_url(),
+}
 
 LOGGER = logging.getLogger("backend.check_stac_metadata.task")
 
 
 def should_log_event_payload() -> None:
-    payload = {
-        DATASET_ID_KEY: any_dataset_id(),
-        VERSION_ID_KEY: any_dataset_version_id(),
-        METADATA_URL_KEY: any_s3_url(),
-    }
-
+    payload = deepcopy(MINIMAL_PAYLOAD)
     expected_log = dumps({"event": payload})
 
-    with patch.object(
-        logging.getLogger("backend.check_stac_metadata.task"), "debug"
-    ) as logger_mock, patch("backend.check_stac_metadata.task.STACDatasetValidator.run"):
-        lambda_handler(
-            payload,
-            any_lambda_context(),
-        )
+    with patch.object(LOGGER, "debug") as logger_mock, patch(
+        "backend.check_stac_metadata.task.STACDatasetValidator.run"
+    ):
+        lambda_handler(payload, any_lambda_context())
+
         logger_mock.assert_any_call(expected_log)
+
+
+@patch("backend.check_stac_metadata.task.validate")
+def should_return_error_when_schema_validation_fails(
+    validate_schema_mock: MagicMock, subtests: SubTests
+) -> None:
+    # Given
+    error_message = any_error_message()
+    error = ValidationError(error_message)
+    validate_schema_mock.side_effect = error
+    expected_log = dumps({ERROR_KEY: error}, default=str)
+
+    with patch.object(LOGGER, "warning") as logger_mock:
+        # When
+        with subtests.test(msg="response"):
+            response = lambda_handler({}, any_lambda_context())
+            # Then
+            assert response == {ERROR_MESSAGE_KEY: error_message}
+
+        # Then
+        with subtests.test(msg="log"):
+            logger_mock.assert_any_call(expected_log)
