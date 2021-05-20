a9791e925b4ef262320ceb7ff4fcdd9f83aec397
Victor Engmark <vengmark@linz.govt.nz>
2021-05-20 14:13:53 +1200

    chore: Remove unreached code

--- backend/pystac_io_methods.py
+++ backend/pystac_io_methods.py
@@ -2,31 +2,22 @@ from typing import IO, Union
 from urllib.parse import urlparse
 
 import boto3
-from pystac import STAC_IO  # type: ignore[import]
-
-from .s3 import S3_SCHEMA
 
 S3_CLIENT = boto3.client("s3")
 
 
 def read_method(uri: str) -> str:
     parsed = urlparse(uri)
-    if parsed.scheme == S3_SCHEMA:
-        bucket = parsed.netloc
-        key = parsed.path[1:]
-        obj = S3_CLIENT.get_object(Bucket=bucket, Key=key)
-        result: str = obj["Body"].read().decode("utf-8")
-    else:
-        result = STAC_IO.default_read_text_method(uri)
+    bucket = parsed.netloc
+    key = parsed.path[1:]
+    obj = S3_CLIENT.get_object(Bucket=bucket, Key=key)
+    result: str = obj["Body"].read().decode("utf-8")
 
     return result
 
 
 def write_method(uri: str, body: Union[bytes, IO[bytes]]) -> None:
     parsed = urlparse(uri)
-    if parsed.scheme == S3_SCHEMA:
-        bucket = parsed.netloc
-        key = parsed.path[1:]
-        S3_CLIENT.put_object(Bucket=bucket, Key=key, Body=body)
-    else:
-        STAC_IO.default_write_text_method(uri, body)
+    bucket = parsed.netloc
+    key = parsed.path[1:]
+    S3_CLIENT.put_object(Bucket=bucket, Key=key, Body=body)
