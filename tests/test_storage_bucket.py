a1646d25d3eec76cae9a9e7158b20cfc19645731
Victor Engmark <vengmark@linz.govt.nz>
2021-06-17 15:46:16 +1200

    refactor: Remove unnecessary test classes (#807)
    Since the relevant resource names are no longer dynamic, we might as
    well just reference them directly.

--- tests/test_storage_bucket.py
+++ tests/test_storage_bucket.py
@@ -1,38 +1,29 @@
-"""
-Geostore Storage Bucket tests.
-"""
-
 from mypy_boto3_s3 import S3Client
 from pytest import mark
 
 from backend.resources import ResourceName
 
 
-class TestWithStorageBucket:
-    storage_bucket_name: str
+@mark.infrastructure
+def should_create_storage_bucket_location_constraint(s3_client: S3Client) -> None:
+    """Test if Geostore Storage S3 Bucket is created in correct region."""
+    response = s3_client.get_bucket_location(Bucket=ResourceName.STORAGE_BUCKET_NAME.value)
+    assert response["LocationConstraint"] == "ap-southeast-2"
 
-    @classmethod
-    def setup_class(cls) -> None:
-        cls.storage_bucket_name = ResourceName.STORAGE_BUCKET_NAME.value
 
-    @mark.infrastructure
-    def should_create_storage_bucket_location_constraint(self, s3_client: S3Client) -> None:
-        """Test if Geostore Storage S3 Bucket is created in correct region."""
-        response = s3_client.get_bucket_location(Bucket=self.storage_bucket_name)
-        assert response["LocationConstraint"] == "ap-southeast-2"
+@mark.infrastructure
+def should_enable_storage_bucket_versioning(s3_client: S3Client) -> None:
+    """Test if Geostore Storage S3 Bucket versioning is enabled."""
+    response = s3_client.get_bucket_versioning(Bucket=ResourceName.STORAGE_BUCKET_NAME.value)
+    assert response["Status"] == "Enabled"
 
-    @mark.infrastructure
-    def should_enable_storage_bucket_versioning(self, s3_client: S3Client) -> None:
-        """Test if Geostore Storage S3 Bucket versioning is enabled."""
-        response = s3_client.get_bucket_versioning(Bucket=self.storage_bucket_name)
-        assert response["Status"] == "Enabled"
 
-    @mark.infrastructure
-    def should_create_storage_bucket_public_access_block(self, s3_client: S3Client) -> None:
-        """Test if Geostore Storage S3 Bucket access is blocked for public."""
-        response = s3_client.get_public_access_block(Bucket=self.storage_bucket_name)
-        public_access_block_configuration = response["PublicAccessBlockConfiguration"]
-        assert public_access_block_configuration["BlockPublicAcls"] is True
-        assert public_access_block_configuration["IgnorePublicAcls"] is True
-        assert public_access_block_configuration["BlockPublicPolicy"] is True
-        assert public_access_block_configuration["RestrictPublicBuckets"] is True
+@mark.infrastructure
+def should_create_storage_bucket_public_access_block(s3_client: S3Client) -> None:
+    """Test if Geostore Storage S3 Bucket access is blocked for public."""
+    response = s3_client.get_public_access_block(Bucket=ResourceName.STORAGE_BUCKET_NAME.value)
+    public_access_block_configuration = response["PublicAccessBlockConfiguration"]
+    assert public_access_block_configuration["BlockPublicAcls"] is True
+    assert public_access_block_configuration["IgnorePublicAcls"] is True
+    assert public_access_block_configuration["BlockPublicPolicy"] is True
+    assert public_access_block_configuration["RestrictPublicBuckets"] is True
