f59150fa687682a45c81931f4411160ecfc89cd8
Mitchell Paff <mpaff@linz.govt.nz>
2021-07-15 13:43:27 +1200

    fix: linting issue

--- tests/test_import_dataset.py
+++ tests/test_import_dataset.py
@@ -230,7 +230,7 @@ def should_batch_copy_files_to_storage(
                 # Then the root asset file is in the root prefix
                 new_root_asset_key = f"{new_prefix}/{root_asset_filename}"
 
-                with subtests.test(msg="Verify root asset contents"), smart_open(
+                with subtests.test(msg="Verify root asset contents"), smart_open.open(
                     f"{storage_bucket_prefix}{new_root_asset_key}"
                 ) as new_root_asset_file:
                     assert root_asset_content == new_root_asset_file.read()
@@ -245,7 +245,7 @@ def should_batch_copy_files_to_storage(
                 # Then the child asset file is in the root prefix
                 new_child_asset_key = f"{new_prefix}/{child_asset_filename}"
 
-                with subtests.test(msg="Verify child asset contents"), smart_open(
+                with subtests.test(msg="Verify child asset contents"), smart_open.open(
                     f"{storage_bucket_prefix}{new_child_asset_key}"
                 ) as new_child_asset_file:
                     assert child_asset_content == new_child_asset_file.read()
