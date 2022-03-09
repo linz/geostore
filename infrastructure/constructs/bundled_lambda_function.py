ecaac094922aa2d964207b7f81b7474b821510a4
Victor Engmark <vengmark@linz.govt.nz>
2022-03-10 10:04:45 +1300

    fix: Bump Lambda resources (#1456)
    * feat: Set Lambda max RAM to 256 MB
    
    This is double the default. All of the jobs were using more than half
    their max RAM even for a simple import, so this might speed things up a
    bit.
    
    * feat: Set import asset file Lambda max RAM to 512 MB
    
    This is quadruple the default, and should hopefully be enough to import
    big files.
    
    * fix: Increase asset file import timeout to max
    
    This job has to wait for potentially large files to be copied across, so
    we might need a good chunk of time.
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- infrastructure/constructs/bundled_lambda_function.py
+++ infrastructure/constructs/bundled_lambda_function.py
@@ -1,12 +1,16 @@
 from typing import Mapping, Optional
 
 from aws_cdk import aws_lambda, aws_lambda_python
-from aws_cdk.core import Construct
+from aws_cdk.core import Construct, Duration
 
 from .backend import BACKEND_DIRECTORY
 from .bundled_code import bundled_code
 from .common import LOG_LEVEL
-from .lambda_config import LAMBDA_TIMEOUT, PYTHON_RUNTIME
+from .lambda_config import (
+    DEFAULT_LAMBDA_MAX_MEMORY_MEGABYTES,
+    DEFAULT_LAMBDA_TIMEOUT,
+    PYTHON_RUNTIME,
+)
 
 
 class BundledLambdaFunction(aws_lambda.Function):
@@ -18,6 +22,8 @@ class BundledLambdaFunction(aws_lambda.Function):
         directory: str,
         extra_environment: Optional[Mapping[str, str]],
         botocore_lambda_layer: aws_lambda_python.PythonLayerVersion,
+        max_memory_megabytes: int = DEFAULT_LAMBDA_MAX_MEMORY_MEGABYTES,
+        timeout: Duration = DEFAULT_LAMBDA_TIMEOUT,
     ):
         environment = {"LOGLEVEL": LOG_LEVEL}
         if extra_environment is not None:
@@ -31,5 +37,6 @@ class BundledLambdaFunction(aws_lambda.Function):
             runtime=PYTHON_RUNTIME,
             environment=environment,
             layers=[botocore_lambda_layer],  # type: ignore[list-item]
-            timeout=LAMBDA_TIMEOUT,
+            timeout=timeout,
+            memory_size=max_memory_megabytes,
         )
