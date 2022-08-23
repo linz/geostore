6ae0273fd54d9cb864a287d17834ca110d53c212
aemad <aemad@linz.govt.nz>
2022-08-23 14:14:25 +1200

    fix: grant permission to import_asset and import_meta

--- infrastructure/constructs/processing.py
+++ infrastructure/constructs/processing.py
@@ -412,7 +412,8 @@ class Processing(Construct):
                     check_files_checksums_single_task.job_role,
                     check_files_checksums_array_task.job_role,
                     populate_catalog_lambda,
-                    import_dataset_task.lambda_function,
+                    import_asset_file_function,
+                    import_metadata_file_function,
                 ],
             }
         )
