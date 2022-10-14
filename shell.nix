0a7c4fab9b0b171d682a3c7c6fc858adfa38a32d
Victor Engmark <vengmark@linz.govt.nz>
2022-10-14 15:14:58 +1300

    build: Move to latest stable nixpkgs
    This should result in fewer breakages during package upgrades, while
    providing a single command to bump the current nixpkgs whenever needed.

--- shell.nix
+++ shell.nix
@@ -1,11 +1,8 @@
 { pkgs ? import
     (
-      fetchTarball
-        {
-          name = "nixos-22.11pre405560.2da64a81275";
-          url = "https://github.com/NixOS/nixpkgs/archive/2da64a81275b68fdad38af669afeda43d401e94b.tar.gz";
-          sha256 = "1k71lmzdaa48yqkmsnd22n177qmxxi4gj2qcmdbv0mc6l4f27wd0";
-        })
+      fetchTarball (
+        builtins.fromJSON (
+          builtins.readFile ./nixpkgs.json)))
     { }
 }:
 let
