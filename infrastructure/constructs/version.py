ca1ac0c211e2807b01c7b82ec31bb93a2cdbc599
Jim Gan <jgan@linz.govt.nz>
2022-12-19 11:53:34 +1300

    refactor: Use GitPython module instead of popen for tag annotation
    An error message matching `fatal: no tag exactly matches <sha1 #>` is thrown when running `cdk synth` or `cdk deploy` locally. This can be misleading as it doesn't explain why a fatal error is thrown, and by what. This problem is caused by `git tag` creating non-annotated tag by default. Rather than calling git commands using Popen, GitPython module provides a much elegant way to interact with git repositories.

--- infrastructure/constructs/version.py
+++ infrastructure/constructs/version.py
@@ -1,12 +1,11 @@
-from subprocess import PIPE, Popen
+from git import Repo  # type: ignore
 
-with Popen(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=PIPE) as branch_command:
-    GIT_BRANCH = branch_command.communicate()[0].decode().strip()
+repo = Repo(search_parent_directories=True)
 
-with Popen(["git", "rev-parse", "--short", "HEAD"], stdout=PIPE) as commit_command:
-    GIT_COMMIT = commit_command.communicate()[0].decode().strip()
+try:
+    GIT_BRANCH = repo.active_branch.name
+except TypeError:
+    GIT_BRANCH = repo.head.object.hexsha
 
-with Popen(["git", "describe", "--tags", "--exact-match"], stdout=PIPE) as tag_command:
-    GIT_TAG = tag_command.communicate()[0].decode().strip()
-if not GIT_TAG:
-    GIT_TAG = "UNRELEASED"
+GIT_COMMIT = repo.git.rev_parse(repo.head, short=True)
+GIT_TAG = next((tag for tag in repo.tags if tag.commit == repo.head.commit), "UNRELEASED")
